c3df028d86565e2ae4044bdc7ff1268183e0aef9
oli-gra <45819636+oli-gra@users.noreply.github.com>
2019-05-19 22:48:05 +0100

    Update LatestMovieReviewsContainer.js

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const GOOG_API_KEY = 'AIzaSyBjl0NQBlQC0LpXSvwEqGS_dqOdULie_NE';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
    + `api-key=${GOOG_API_KEY}`;
 
